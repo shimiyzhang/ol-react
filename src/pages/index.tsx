eb56f32f0d1259e195f03660f9531065bfea0914
张书猛 <916406821@qq.com>
2022-09-21 14:16:05 +0800

    import ol, render map.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
-import styles from './index.less';
+import React, { useEffect } from 'react';
+import Map from 'ol/Map';
+import Tile from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import View from 'ol/View';
+import { fromLonLat } from 'ol/proj';
+import './index.less';
 
-export default function IndexPage() {
-  return (
-    <div>
-      <h1 className={styles.title}>Page index</h1>
-    </div>
-  );
-}
+const Index: React.FC = () => {
+  useEffect(() => {
+    let map = new Map({
+      // 设置挂载点为map
+      target: 'map',
+      // 设置图层
+      layers: [
+        new Tile({
+          source: new OSM(),
+        }),
+      ],
+      // 设置地图的可视区域，center为中心点，zoom为缩放的层级
+      view: new View({
+        center: fromLonLat([116.2, 39.56]),
+        zoom: 5,
+      }),
+    });
+  });
+
+  return <div id="map"></div>;
+};
+
+export default Index;
