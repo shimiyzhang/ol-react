a88c485f5065b72e217588fc3d5ed7740563bd8c
张书猛 <916406821@qq.com>
2022-09-21 17:01:19 +0800

    隐藏默认控件，设置旋转交互

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,19 @@
 import React, { useEffect } from 'react';
+import './index.less';
 import Map from 'ol/Map';
 import Tile from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import View from 'ol/View';
 import { fromLonLat } from 'ol/proj';
-import './index.less';
+import { defaults as defaultControls } from 'ol/control';
+import {
+  defaults as defaultInteractions,
+  DragRotateAndZoom,
+} from 'ol/interaction';
 
 const Index: React.FC = () => {
   useEffect(() => {
-    let map = new Map({
+    const map = new Map({
       // 设置挂载点为map
       target: 'map',
       // 设置图层
@@ -21,7 +26,16 @@ const Index: React.FC = () => {
       view: new View({
         center: fromLonLat([116.2, 39.56]),
         zoom: 5,
+        minZoom: 3,
+        maxZoom: 10,
+        rotation: 1, // 初始旋转（以弧度为单位）(顺时针正旋转，0表示北)
       }),
+      controls: defaultControls({
+        zoom: false,
+        rotate: false,
+        attribution: false,
+      }),
+      interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
     });
   });
 
